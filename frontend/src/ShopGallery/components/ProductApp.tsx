eb57987977f9d5c7a9046183037ab4acc8bab71d
devBK84 <bk@kharashfeh.com>
2023-01-03 11:06:19 +0100

    Update shop

---
+++ frontend/src/ShopGallery/components/ProductApp.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+import {Product} from "../model/Product";
+import axios from "axios";
+import ProductGallery from "./ProductGallery";
+
+export default function ProductApp(){
+    const [products, setProducts] = useState<Product[]>([])
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    function getProducts() {
+        axios.get("/api/products").then((response) => {
+            setProducts(response.data)
+        }).catch(error => console.error(error))
+    }
+    return (
+        <div>
+            <ProductGallery products={products}/>
+        </div>
+    )
+}
---
+++ frontend/src/ShopGallery/components/ProductGallery.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Product} from "../model/Product";
+import ProductCard from "./ProductCard";
+
+
+type ShopGalleryProps = {
+    products: Product[]
+
+}
+
+export default function ProductGallery(props: ShopGalleryProps){
+
+
+    const productsCards = props.products.map(product =>{
+        return (
+            <ProductCard product={product} key={product.id}/>
+        )
+    })
+
+    return(
+        <div>
+            {productsCards}
+        </div>
+    )
+}
