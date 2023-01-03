9a1c8c0caa5071c59430e62fcbb5d087459a0ee3
devBK84 <bk@kharashfeh.com>
2023-01-03 19:12:48 +0100

    Install react-dom, styling WelcomeScreen.css & update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import WelcomeScreen from "./ShopGallery/components/WelcomeScreen";
 import ProductApp from "./ShopGallery/components/ProductApp";
@@ -10,12 +11,15 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-            <ProductApp/>
-            </header>
-            <WelcomeScreen></WelcomeScreen>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<WelcomeScreen/>}></Route>
+                    <Route path={"/products"} element={<ProductApp/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
-    );
+);
 }
 
 export default App;
--- frontend/src/ShopGallery/components/WelcomeScreen.tsx
+++ frontend/src/ShopGallery/components/WelcomeScreen.tsx
@@ -4,9 +4,17 @@ import "../../App.css"
 import hintergrund from "../../static/img/hintergrund@1x.png"
 import banderole_Logo from "../../static/img/banderole-logo-1@1x.png"
 import citkar2502225 from "../../static/img/citkar-250222-5-1@1x.png"
+import {useNavigate} from "react-router-dom";
+
+
 
 export default function WelcomeScreen() {
 
+    const navigate = useNavigate()
+
+    function handleDetailsClick(){
+        navigate("/products")
+    }
 
     return (
         <div className="container-center-horizontal">
@@ -15,9 +23,9 @@ export default function WelcomeScreen() {
                     <img className="hintergrund" src={hintergrund} alt="Hintergrund"/>
                     <img className="banderole_-logo" src={banderole_Logo} alt="Banderole_Logo"/>
                     <img className="cit-kar-250222-5" src={citkar2502225} alt="CitKar-250222-5"/>
-                    <h1 className="title">The Future of (non) Dairy</h1>
+                    <h1 className="title">THE FUTURE <br/> OF (NON) DAIRY</h1>
                 </div>
-                <div className="x941-am"></div>
+                <button onClick={handleDetailsClick} className="button">Los geht's</button>
             </div>
         </div>
     );
