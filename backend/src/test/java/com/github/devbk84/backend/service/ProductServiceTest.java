9bc02e03a42acef38327a4c4448c2c52daa211f9
devBK84 <bk@kharashfeh.com>
2023-01-05 18:14:05 +0100

    Add ProductServiceTest.java

--- backend/src/test/java/com/github/devbk84/backend/service/ProductServiceTest.java
+++ backend/src/test/java/com/github/devbk84/backend/service/ProductServiceTest.java
@@ -3,8 +3,11 @@ package com.github.devbk84.backend.service;
 import com.github.devbk84.backend.models.Product;
 import com.github.devbk84.backend.repo.ProductRepo;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -28,5 +31,26 @@ class ProductServiceTest {
         // THEN
         assertEquals(expected, result);
         verify(productRepo).findAll();
+    }
+
+    @Test
+    void getProductsById(){
+        // GIVEN
+        Product expected = new Product(
+                "69",
+                "Milch",
+                "Test",
+                "orderFav",
+                new BigDecimal("1.50")
+        );
+        productRepo.save(expected);
+
+        // WHEN
+        when(productRepo.findById("69")).thenReturn(Optional.of(expected));
+        Product actual = productService.getProductsById("69");
+
+        // THEN
+        assertEquals(expected, actual);
+        verify(productRepo).findById("69");
     }
 }
