09c9fbc4d0468776d439f43cc6b826a564f51ed5
devBK84 <bk@kharashfeh.com>
2023-01-05 17:17:25 +0100

    Add ProductControllerTest & Update ProductController

---
+++ backend/src/main/java/com/github/devbk84/backend/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.github.devbk84.backend.controller;
+
+import com.github.devbk84.backend.models.Product;
+import com.github.devbk84.backend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+
+        return productService.getAllProducts();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product getProductsById(@PathVariable String id){
+        return productService.getProductsById(id);
+    }
+
+
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity <Product> updateProduct(
+//            @PathVariable String id,
+//            @RequestBody Product shopService) {
+//        return shopService.equals(id, updateProduct());
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Product> deleteProduct(@PathVariable String id) {
+//        return new ResponseEntity<>(new Product(id));
+//    }
+}
--- backend/src/main/java/com/github/devbk84/backend/repo/ProductRepo.java
+++ backend/src/main/java/com/github/devbk84/backend/repo/ProductRepo.java
@@ -3,7 +3,10 @@ package com.github.devbk84.backend.repo;
 import com.github.devbk84.backend.models.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+
 @Repository
 public interface ProductRepo extends MongoRepository<Product, String> {
 
 }
+
