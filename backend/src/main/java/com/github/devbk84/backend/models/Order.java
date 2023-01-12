6f2535ed80c450c583912fd1449eb1c4edff2b8b
devBK84 <bk@kharashfeh.com>
2023-01-12 14:59:11 +0100

    Create OrderService, OrderRepo, OrderController, OrderDTO

--- backend/src/main/java/com/github/devbk84/backend/models/Order.java
+++ backend/src/main/java/com/github/devbk84/backend/models/Order.java
@@ -1,8 +1,12 @@
 package com.github.devbk84.backend.models;
 
-import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+@Document("order")
 public record Order(
+        @Id
         String id,
         String payment,
         List<Product> products,
