8a84d4ee030293880673382bbe55bef2237daf9f
devBK84 <bk@kharashfeh.com>
2023-01-17 15:42:29 +0100

    Update ProductCard.tsx

--- frontend/src/ShopGallery/components/ProductCard.tsx
+++ frontend/src/ShopGallery/components/ProductCard.tsx
@@ -4,6 +4,7 @@ import "./ProductCard.css"
 import bild_milch from "../../static/img/bild-milch-1@1x.png"
 import bild_laktosefrei from "../../static/img/bild-laktosefrei-1@1x.png"
 import bild_joghurt from "../../static/img/bild-joghurt-1@1x.png"
+import NavBar from "./NavBar/NavBar";
 
 
 export default function ProductCard() {
@@ -11,12 +12,12 @@ export default function ProductCard() {
     return (
         <div>
             <div className="Article-Milk-Products">
+                <div className="bg-carousel"></div>
                 <div className="milk-container"/>
                 <div className="milch">
                     MILCH
                 </div>
                 <img className="bild_-milch" src={bild_milch} alt="bild-milch"/>
-
 
                 <div className="laktosefrei-container"/>
                 <div className="laktosefrei">
@@ -24,13 +25,13 @@ export default function ProductCard() {
                 </div>
                 <img className="bild_-laktosefrei" src={bild_laktosefrei} alt="bild-laktosefrei"/>
 
-
                 <div className="joghurt-container">
                     <div className="joghurt">
                         JOGHURT
                     </div>
                     <img className="bild_-joghurt" src={bild_joghurt} alt="bild-joghurt"/>
                 </div>
+                <NavBar></NavBar>;
             </div>
 
         </div>
