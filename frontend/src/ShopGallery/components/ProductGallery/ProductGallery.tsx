f67a167b8661652ec8fc90c3e45c6a5309eb78a7
devBK84 <bk@kharashfeh.com>
2023-01-19 17:22:24 +0100

    Update ProductGallery.css

---
+++ frontend/src/ShopGallery/components/ProductGallery/ProductGallery.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Product} from "../../model/Product";
+import ProductCard from "../ProductCard/ProductCard";
+import "../ProductGallery/ProductGallery.css";
+import background from "../../../static/img/path-567@1x.png"
+
+
+type ShopGalleryProps = {
+    products: Product[]
+
+}
+
+export default function ProductGallery(props: ShopGalleryProps){
+
+
+    const productsCards = props.products.map(product =>{
+        return (
+            <ProductCard key={product.id} product = {product}/>
+        )
+    })
+
+    return(
+        <div className={"background"}style={{backgroundImage:`url(${background})`}}>
+            {productsCards}
+        </div>
+    )
+}
