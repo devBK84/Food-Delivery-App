970764c5d5779564cfe3261d7f1529f26ba792f0
devBK84 <bk@kharashfeh.com>
2023-01-20 16:27:45 +0100

    Update Header, NavBar & add Mui

--- frontend/src/ShopGallery/components/Header/Header.tsx
+++ frontend/src/ShopGallery/components/Header/Header.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import "../NavBar/NavBar.css"
 import background from "../../../static/img/path-566-10@1x.png"
 import "../Header/Header.css"
+import {ArrowBack} from "@mui/icons-material";
+
 
 
 export default function Header() {
 
     return (
         <div className="header">
+            <div className={"arrow1"}>
+                <ArrowBack></ArrowBack>
+            </div>
             MILCH
+
             <div className={"background"} style={{backgroundImage: `url(${background})`}}>
 
             </div>
--- frontend/src/ShopGallery/components/ProductCard/ProductCard.tsx
+++ frontend/src/ShopGallery/components/ProductCard/ProductCard.tsx
@@ -16,11 +16,14 @@ export default function ProductCard(props: ProductCardProps) {
         <div>
             <div className="Article-Milk-Products">
                 <div className="bg-carousel"></div>
-                <div className="container"/>
-                {props.product.name}
+                <div className="container">
+                    <img className="img"
+                         src={"http://localhost:8080/bild-" + props.product.shortname.toLowerCase() + ".png"}
+                         alt={props.product.shortname}/>
+                </div>
+
             </div>
-            <img className="img" src={"http://localhost:8080/bild-" + props.product.shortname.toLowerCase() + ".png"}
-                 alt={props.product.shortname}/>
+
         </div>
     );
 }
