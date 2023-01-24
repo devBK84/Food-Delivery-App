994e26d20db6cb7b31d16cdf357cc1c429e16480
devBK84 <bk@kharashfeh.com>
2023-01-24 09:27:46 +0100

    Fixed Columns

--- backend/src/main/java/com/github/devbk84/backend/models/Order.java
+++ backend/src/main/java/com/github/devbk84/backend/models/Order.java
@@ -4,13 +4,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+
 @Document("order")
 public record Order(
         @Id
         String id,
         String payment,
         List<Product> products,
-
         String ordertBy
 
 ) {
