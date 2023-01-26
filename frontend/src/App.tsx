e1d993ce8ea36184fd9b9039e4f2cc2a929e80ab
devBK84 <bk@kharashfeh.com>
2023-01-26 16:46:40 +0100

    Add CardProduct.ts & Create minus & plus function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,17 +8,46 @@ import ProductDetail from "./ShopGallery/components/ProductDetail/ProductDetail"
 import CheckOut from "./ShopGallery/components/CheckOut/CheckOut";
 import {Product} from "./ShopGallery/model/Product";
 import axios from "axios";
+import {CardProduct} from "./ShopGallery/model/CardProduct";
 
 
 function App() {
-    const [cardProducts, setCardProducts] = useState<Product[]>([])
+    const [cardProducts, setCardProducts] = useState<CardProduct[]>([])
 
     function addProductToCard(product: Product) {
-        setCardProducts([
-            ...cardProducts,
-            product
-        ])
+        if (cardProducts.map(cardProduct => cardProduct.product.id).includes(product.id)) {
+            setCardProducts(prevState => {
+                return prevState.map(cardProduct => {
+                    if (cardProduct.product.id === product.id) {
+                        cardProduct.count++;
+
+                    }
+                    return cardProduct
+                })
+            })
+        } else {
+            setCardProducts(prevState => [...prevState, {product: product, count: 1}])
+        }
+
     }
+
+    function minusProductInCard(product: Product) {
+        if (cardProducts.map(cardProduct => cardProduct.product.id).includes(product.id)) {
+            setCardProducts(prevState => {
+                return prevState.map(cardProduct => {
+                    if (cardProduct.product.id === product.id) {
+                        cardProduct.count--;
+
+                    }
+                    return cardProduct
+                })
+            })
+        } else {
+            setCardProducts(prevState => [...prevState, {product: product, count: 1}])
+        }
+
+    }
+
 
     const [products, setProducts] = useState<Product[]>([])
     const getProducts = useCallback(() => {
@@ -32,6 +61,14 @@ function App() {
         getProducts()
     }, [getProducts])
 
+    function handleDeleteChange(id: string) {
+        setCardProducts(prevState => {
+            return prevState.filter(product => {
+                return product.product.id !== id
+            })
+        })
+    }
+
     return (
         <div className="App">
 
@@ -42,7 +79,8 @@ function App() {
                     <Route path={"/products"}
                            element={<ProductApp amountArticles={cardProducts.length} products={products}/>}/>
                     <Route path={"/checkout/"}
-                           element={<CheckOut products={cardProducts} amountArticles={cardProducts.length}/>}></Route>
+                           element={<CheckOut addProductToCard = {addProductToCard} minusProductInCard={minusProductInCard} handleDeleteChange={handleDeleteChange} products={cardProducts}
+                                              amountArticles={cardProducts.length}/>}></Route>
                     <Route path={"/products/:id"}
                            element={<ProductDetail amountArticles={cardProducts.length}
                                                    handleCardProduct={addProductToCard}/>}></Route>
---
+++ frontend/src/ShopGallery/model/CardProduct.ts
@@ -0,0 +1,7 @@
+import {Product} from "./Product";
+
+export type CardProduct = {
+    product: Product,
+    count: number
+
+}
